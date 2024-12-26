068021ded666a181150354b85e6dad35613fb971
Guilherme Nabor <57880169+GuilhermesNabor@users.noreply.github.com>
2024-12-26 13:17:31 -0300

    Add files via upload

---
+++ time.js
@@ -0,0 +1,15 @@
+function calcularTempo() {
+    const dataInicio = new Date('2024-11-22');
+    const dataAtual = new Date();
+    const tempo = dataAtual - dataInicio;
+
+    const dias = Math.floor(tempo / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((tempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((tempo % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((tempo % (1000 * 60)) / 1000);
+
+    document.getElementById('time-elapsed').textContent = 
+        `Nos conhecemos há ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos.`;
+}
+
+setInterval(calcularTempo, 1000);
